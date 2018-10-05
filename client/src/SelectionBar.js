2a887f2f7d1633af8e1e2b168d4969ded13e6e2c
Nick Bourdakos <bourdakos1@gmail.com>
2018-10-05 18:41:26 -0400

    make touch target larger

--- client/src/SelectionBar.js
+++ client/src/SelectionBar.js
@@ -47,65 +47,74 @@ class SelectionBar extends Component {
     })
     return (
       <div className={`SelectionBar ${selectionCount > 0 ? '--Active' : ''}`}>
-        <div className="SelectionBar-count">{`${selectionCount} selected`}</div>
+        <div className="SelectionBar-Wrapper">
+          <div className="SelectionBar-count">{`${selectionCount} selected`}</div>
+        </div>
         <div
-          className={`SelectionBar-DropDown ${
-            this.state.showDropDown ? '--Active' : ''
-          }`}
-          ref={input => {
-            this.dropDownRef = input
-          }}
+          className="SelectionBar-Wrapper SelectionBar-Button"
           onClick={this.showDropDown}
         >
-          Label{' '}
-          <svg
-            className="SelectionBar-dropdown-Icon"
-            width="10"
-            height="5"
-            viewBox="0 0 10 5"
-          >
-            <path d="M0 0l5 4.998L10 0z" />
-          </svg>
-          <input
-            className="SelectionBar-DropDown-Filter"
-            placeholder="Filter"
+          <div
+            className={`SelectionBar-DropDown ${
+              this.state.showDropDown ? '--Active' : ''
+            }`}
             ref={input => {
-              this.filterFieldRef = input
+              this.dropDownRef = input
             }}
-          />
-          <div className="SelectionBar-DropDown-Menu">
-            {onlyLabels.map(section => {
-              return (
-                <div
-                  className="SelectionBar-DropDown-MenuItemWrapper"
-                  onClick={() => {
-                    labelImages(section)
-                  }}
-                >
-                  <div className="SelectionBar-DropDown-MenuItem">
-                    {section}
-                  </div>
-                </div>
-              )
-            })}
-            <div
-              className="SelectionBar-DropDown-MenuItemWrapper"
-              onClick={() => {
-                labelImages('Unlabeled')
-              }}
+          >
+            Label{' '}
+            <svg
+              className="SelectionBar-dropdown-Icon"
+              width="10"
+              height="5"
+              viewBox="0 0 10 5"
             >
-              <div className="SelectionBar-DropDown-MenuItem">Unlabel</div>
+              <path d="M0 0l5 4.998L10 0z" />
+            </svg>
+            <input
+              className="SelectionBar-DropDown-Filter"
+              placeholder="Filter"
+              ref={input => {
+                this.filterFieldRef = input
+              }}
+            />
+            <div className="SelectionBar-DropDown-Menu">
+              {onlyLabels.map(section => {
+                return (
+                  <div
+                    className="SelectionBar-DropDown-MenuItemWrapper"
+                    onClick={() => {
+                      labelImages(section)
+                    }}
+                  >
+                    <div className="SelectionBar-DropDown-MenuItem">
+                      {section}
+                    </div>
+                  </div>
+                )
+              })}
+              <div
+                className="SelectionBar-DropDown-MenuItemWrapper"
+                onClick={() => {
+                  labelImages('Unlabeled')
+                }}
+              >
+                <div className="SelectionBar-DropDown-MenuItem">Unlabel</div>
+              </div>
             </div>
           </div>
         </div>
-        <div onClick={deleteImages}>Delete</div>
-        <div className="SelectionBar-close-IconWrapper" onClick={deselectAll}>
-          <svg
-            className="SelectionBar-close-Icon"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-          >
+        <div
+          className="SelectionBar-Wrapper SelectionBar-Button"
+          onClick={deleteImages}
+        >
+          <div>Delete</div>
+        </div>
+        <div
+          className="SelectionBar-close-IconWrapper SelectionBar-Wrapper SelectionBar-Button"
+          onClick={deselectAll}
+        >
+          <svg className="SelectionBar-close-Icon" viewBox="5 5 10 10">
             <path d="M10 9.293l4.146-4.147.708.708L10.707 10l4.147 4.146-.708.708L10 10.707l-4.146 4.147-.708-.708L9.293 10 5.146 5.854l.708-.708L10 9.293z" />
           </svg>
         </div>
