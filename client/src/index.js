da7ab21d7aa83ccc42352a3f475b8e226d8aaec4
Nick Bourdakos <bourdakos1@gmail.com>
2018-12-05 13:51:13 -0500

    add react google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Routing from './Routing'
 import registerServiceWorker from './registerServiceWorker'
+import ReactGA from 'react-ga'
+
+ReactGA.initialize('UA-130502274-1')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 ReactDOM.render(<Routing />, document.getElementById('root'))
 registerServiceWorker()
