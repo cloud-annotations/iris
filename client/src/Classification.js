afaed29da55e7ca16824de856af96f65b205eb6a
Nick Bourdakos <bourdakos1@gmail.com>
2018-12-27 11:41:40 -0500

    move the grid down so it doesn't get covered

--- client/src/Classification.js
+++ client/src/Classification.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import fetchImages from 'api/fetchImages'
 import GridController from 'common/Grid/GridController'
 import ImageTile from './ImageTile'
 import EmptySet from './EmptySet'
 import SelectionBar from './SelectionBar'
-import Sidebar, { ALL_IMAGES, UNLABELED, LABELED } from './Sidebar'
+import { ALL_IMAGES, UNLABELED, LABELED } from './Sidebar'
 import './App.css'
 
 export default class App extends Component {
@@ -21,11 +20,19 @@ export default class App extends Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.currentSection !== this.props.currentSection) {
-      this.deselectAll()
+      this.handleDeselectAll()
     }
   }
 
-  deselectAll = () => {
+  // MARK: - Event listeners
+
+  handleSelectionChanged = selection => {
+    this.setState({
+      selection: selection
+    })
+  }
+
+  handleDeselectAll = () => {
     this.setState(prevState => {
       const selection =
         prevState.selection && prevState.selection.map(() => false)
@@ -36,174 +43,11 @@ export default class App extends Component {
     })
   }
 
-  // labelImages = newLabel => {
-  //   const promise = new Promise((resolve, reject) => {
-  //     this.setState(
-  //       prevState => {
-  //         let newCollection = { ...prevState.collection }
-  //
-  //         let aNewArrayOfThingsToBeAdded = []
-  //
-  //         let count = 0
-  //         prevState.labelList.forEach(label => {
-  //           const section = [...prevState.collection[label]]
-  //           const newSection = section.filter((imageName, i) => {
-  //             if (prevState.selection[i + count]) {
-  //               // If the image is selected:
-  //
-  //               // Copy the image to the new label in the collection
-  //               aNewArrayOfThingsToBeAdded = [
-  //                 ...aNewArrayOfThingsToBeAdded,
-  //                 section[i]
-  //               ]
-  //
-  //               // Don't include it in the new section
-  //               return false
-  //             }
-  //             return true
-  //           })
-  //
-  //           // Replace the current section with the filtered section.
-  //           newCollection[label] = newSection
-  //           count += section.length
-  //         })
-  //
-  //         newCollection[newLabel] = [
-  //           ...newCollection[newLabel],
-  //           ...aNewArrayOfThingsToBeAdded
-  //         ]
-  //
-  //         return {
-  //           collection: newCollection,
-  //           selection: prevState.selection.map(() => false),
-  //           lastSelected: null
-  //         }
-  //       },
-  //       () => {
-  //         this.annotationsToCsv()
-  //           .then(resolve)
-  //           .catch(reject)
-  //       }
-  //     )
-  //   })
-  //
-  //   this.changeRequest(promise)
-  // }
+  handleLabelImages = () => {}
 
-  // deleteImages = () => {
-  //   let deleteRequests = []
-  //   this.setState(
-  //     prevState => {
-  //       let newCollection = { ...prevState.collection }
-  //
-  //       let count = 0
-  //       prevState.labelList.forEach(label => {
-  //         const section = [...prevState.collection[label]]
-  //         const newSection = section.filter((imageName, i) => {
-  //           if (prevState.selection[i + count]) {
-  //             // If the image is selected:
-  //             // Delete it from server.
-  //             const url = `api/proxy/${localStorage.getItem('loginUrl')}/${
-  //               this.props.bucket
-  //             }/${imageName}`
-  //             const options = {
-  //               method: 'DELETE'
-  //             }
-  //             const request = new Request(url)
-  //             const deleteRequest = fetch(request, options)
-  //               .then(response => {
-  //                 console.log(response)
-  //               })
-  //               .catch(error => {
-  //                 console.error(error)
-  //               })
-  //
-  //             deleteRequests = [...deleteRequests, deleteRequest]
-  //             // Don't include it in the new section
-  //             return false
-  //           }
-  //           return true
-  //         })
-  //
-  //         // Replace the current section with the filtered section.
-  //         newCollection[label] = newSection
-  //         count += section.length
-  //       })
-  //
-  //       const newSelection = Object.keys(newCollection).reduce((acc, key) => {
-  //         return [...acc, ...newCollection[key].map(() => false)]
-  //       }, [])
-  //
-  //       return {
-  //         collection: newCollection,
-  //         selection: newSelection,
-  //         lastSelected: null
-  //       }
-  //     },
-  //     () => {
-  //       const changes = Promise.all(deleteRequests)
-  //       changes.then(() => this.annotationsToCsv())
-  //       this.changeRequest(changes)
-  //     }
-  //   )
-  // }
+  handleDeleteImages = () => {}
 
-  // labelsToCsv = () => {
-  //   const csvFile = this.state.labelList
-  //     .filter(label => {
-  //       return label !== 'Unlabeled'
-  //     })
-  //     .reduce((acc, label) => {
-  //       return acc + label + '\r\n'
-  //     }, '')
-  //
-  //   const blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' })
-  //
-  //   const url = `api/proxy/${localStorage.getItem('loginUrl')}/${
-  //     this.props.bucket
-  //   }/_labels.csv`
-  //   const options = {
-  //     method: 'PUT',
-  //     body: blob
-  //   }
-  //   const request = new Request(url)
-  //   return fetch(request, options)
-  // }
-  //
-  // annotationsToCsv = () => {
-  //   const csvFile = this.state.labelList
-  //     .filter(label => {
-  //       return label !== 'Unlabeled'
-  //     })
-  //     .reduce((acc, label) => {
-  //       return (
-  //         acc +
-  //         this.state.collection[label].reduce((acc, url) => {
-  //           return acc + url + ',' + label + '\r\n'
-  //         }, '')
-  //       )
-  //     }, '')
-  //
-  //   console.log(csvFile)
-  //
-  //   const blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' })
-  //
-  //   const url = `api/proxy/${localStorage.getItem('loginUrl')}/${
-  //     this.props.bucket
-  //   }/_annotations.csv`
-  //   const options = {
-  //     method: 'PUT',
-  //     body: blob
-  //   }
-  //   const request = new Request(url)
-  //   return fetch(request, options)
-  // }
-
-  onSelectionChanged = selection => {
-    this.setState({
-      selection: selection
-    })
-  }
+  handleCreateLabel = () => {}
 
   // MARK: - Render method
 
@@ -227,18 +71,15 @@ export default class App extends Component {
       return acc
     }, {})
 
-    console.log(visibleLabels)
-    console.log(visibleImages)
-
     return (
       <div>
         <SelectionBar
           selectionCount={selectionCount}
           sections={this.props.collection.labels}
-          deselectAll={this.deselectAll}
-          labelImages={this.labelImages}
-          createLabel={this.createLabel}
-          deleteImages={this.deleteImages}
+          deselectAll={this.handleDeselectAll}
+          labelImages={this.handleLabelImages}
+          createLabel={this.handleCreateLabel}
+          deleteImages={this.handleDeleteImages}
         />
         <EmptySet
           show={
@@ -252,7 +93,7 @@ export default class App extends Component {
           sections={visibleLabels}
           collection={visibleImages}
           selection={this.state.selection}
-          onSelectionChanged={this.onSelectionChanged}
+          onSelectionChanged={this.handleSelectionChanged}
           gridItem={<ImageTile bucket={this.props.bucket} />}
         />
       </div>
