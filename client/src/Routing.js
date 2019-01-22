bec292e92e8ded31c675f63c4576e72f969bcbe8
Nick Bourdakos <bourdakos1@gmail.com>
2019-01-22 15:56:03 -0500

    add session ticker back

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -5,10 +5,30 @@ import App from './App'
 import Login from './Login'
 import Buckets from './Buckets'
 import history from './history'
+import { validateCookies } from './Utils'
 
 class Routing extends Component {
   state = {
+    intervalID: null,
     buckets: null
+  }
+
+  componentDidMount() {
+    const intervalID = setInterval(() => {
+      console.log('tic toc')
+      validateCookies().catch(error => {
+        if (error.message === 'Forbidden') {
+          history.push('/login')
+        }
+      })
+    }, 10 * 1000)
+    this.setState({
+      intervalID: intervalID
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalID)
   }
 
   cacheBucketList = buckets => {
