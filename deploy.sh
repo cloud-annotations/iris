14730c6ab9fbc1c55a3332303e319aacbcd846bc
Nick Bourdakos <bourdakos1@gmail.com>
2019-06-11 14:00:32 -0400

    refactor kubernetes

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
+# export KUBECONFIG="/Users/niko/.bluemix/plugins/container-service/clusters/annotations/kube-config-wdc04-annotations.yml"
+# export KUBECONFIG="/Users/niko/.bluemix/plugins/container-service/clusters/staging.annotations/kube-config-wdc04-staging.annotations.yml"
+
 if [ "$DEPLOY_TO" = "production" ]
 then
   # PRODUCTION:
   echo "Deploying to PRODUCTION..."
-  export KUBECONFIG="/Users/niko/.bluemix/plugins/container-service/clusters/annotations/kube-config-wdc04-annotations.yml"
   CLUSTER="annotations"
   URL="https://${CLUSTER}.us-east.containers.appdomain.cloud"
-  DEPLOYMENT="cloud-annotations-production"
 else
   # STAGING:
   echo "Deploying to STAGING..."
-  export KUBECONFIG="/Users/niko/.bluemix/plugins/container-service/clusters/staging.annotations/kube-config-wdc04-staging.annotations.yml"
   CLUSTER="staging.annotations"
   URL="https://stagingannotations.us-east.containers.appdomain.cloud"
-  DEPLOYMENT="cloud-annotations-staging"
 fi
 
-PROJECT_ID="bourdakos1"
-NAME="annotate"
-IMAGE_NAME="us.icr.io/${PROJECT_ID}/${NAME}:$(git rev-parse HEAD)"
+IMAGE_NAME="us.icr.io/bourdakos1/annotate:$(git rev-parse HEAD)"
 
 function fail {
   echo $1 >&2
@@ -31,15 +28,20 @@ trap 'fail "The deployment was aborted. Message -- "' ERR
 function configure {
   echo "Validating configuration..."
   [ ! -z "$CLUSTER" ] || fail "Configuration option is not set: CLUSTER"
-  [ ! -z "$DEPLOYMENT" ] || fail "Configuration option is not set: DEPLOYMENT"
-  [ ! -z "$PROJECT_ID" ] || fail "Configuration option is not set: PROJECT_ID"
   [ ! -z "$IMAGE_NAME" ] || fail "Configuration option is not set: IMAGE_NAME"
-  # ibmcloud login -sso
+  
+  ACCOUNT_ID="$(ibmcloud account show)"
+  if [[ $ACCOUNT_ID != *"47b84451ab70b94737518f7640a9ee42"* ]]; then
+    ibmcloud login -sso
+  fi
 }
 
 function download_config {
   echo Downloading config for $CLUSTER ...
-  ibmcloud cs cluster-config $CLUSTER
+  CONFIG="$(ibmcloud cs cluster-config $CLUSTER)"
+  CONFIG=${CONFIG##*export KUBECONFIG=}
+  CONFIG=${CONFIG%%.yml*}
+  export KUBECONFIG=$CONFIG.yml
 }
 
 function attempt_build {
@@ -49,9 +51,8 @@ function attempt_build {
 
 function set_image {
   echo Container build completed, updating $DEPLOYMENT ...
-  # kubectl run $DEPLOYMENT --image=$IMAGE_NAME # Must only be run if this is a new DEPLOYMENT
-  kubectl set image deployments/$DEPLOYMENT $DEPLOYMENT=$IMAGE_NAME
-  kubectl scale deployment $DEPLOYMENT --replicas=6
+  sed -i '' "s,\(^.*image: \)\(.*$\),\1"$IMAGE_NAME"," k8s/frontend.yaml
+  kubectl apply -f k8s
 }
 
 configure
--- generate-secret.sh
+++ generate-secret.sh
@@ -11,4 +11,4 @@ metadata:
 type: Opaque
 data:
   tls.crt: ""$stripped_cert""
-  tls.key: ""$stripped_key""" > secret.yaml
+  tls.key: ""$stripped_key""" > k8s/secret.yaml
