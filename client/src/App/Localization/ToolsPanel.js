c90d9c8169147057b68344a1d8a0df87168f67d4
Nick Bourdakos <bourdakos1@gmail.com>
2019-06-28 14:27:57 -0400

    start adding tools

--- client/src/App/Localization/ToolsPanel.js
+++ client/src/App/Localization/ToolsPanel.js
@@ -3,7 +3,21 @@ import React from 'react'
 import styles from './ToolsPanel.module.css'
 
 const ToolsPanel = () => {
-  return <div className={styles.wrapper}>{''}</div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.tool}>
+        <svg className={styles.move} width="20" height="20" viewBox="0 0 40 40">
+          <path d="M19,11h2V29H19V11Zm-8,8H29v2H11V19ZM21,35H19l-4-6H25ZM35,19v2l-6,4V15ZM5,21V19l6-4V25ZM19,5h2l4,6H15Z" />
+        </svg>
+      </div>
+
+      <div className={styles.tool}>
+        <svg className={styles.box} width="20" height="20" viewBox="0 0 40 40">
+          <rect x="4" y="8" width="32" height="24" />
+        </svg>
+      </div>
+    </div>
+  )
 }
 
 export default ToolsPanel
