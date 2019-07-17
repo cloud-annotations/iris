ee4905277cab60447270c4c188a405705431c674
Nick Bourdakos <bourdakos1@gmail.com>
2019-07-17 14:01:26 -0400

    start styling appbar

--- client/src/App/AppBar.js
+++ client/src/App/AppBar.js
@@ -38,6 +38,15 @@ const AppBar = ({ profile }) => {
           <path d="M11.17 6l3.42 3.41.58.59H28v16H4V6h7.17m0-2H4a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2H16l-3.41-3.41A2 2 0 0 0 11.17 4z" />
         </svg>
       </div>
+      <div className={styles.headerWrapper}>
+        <div className={styles.bucketName}>chumbucket</div>
+        <div className={styles.options}>
+          <div className={styles.option}>File</div>
+          <div className={styles.option}>Edit</div>
+          <div className={styles.option}>Image</div>
+          <div className={styles.saved}>Saved</div>
+        </div>
+      </div>
       <Toggle
         className={styles.toggle}
         checked={darkModeToggle}
