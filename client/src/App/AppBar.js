b8e51fcefe6b2927aeb3a799eeca581997c9cc9f
Nick <bourdakos1@gmail.com>
2019-09-08 19:21:19 -0400

    add create ml annotations

--- client/src/App/AppBar.js
+++ client/src/App/AppBar.js
@@ -29,13 +29,24 @@ const generateFiles = async (images, videos) => {
 
 const zipImages = async (bucket, collection, folder) => {
   const labeledImageNames = Object.keys(collection.annotations)
-  await Promise.all(
+  return await Promise.all(
     labeledImageNames.map(async name => {
       const imgData = await new COS({ endpoint: defaultEndpoint }).getObject({
         Bucket: bucket,
         Key: name
       })
       folder.file(name, imgData, { binary: true })
+
+      const image = new Image()
+      image.src = URL.createObjectURL(imgData)
+      return await new Promise(resolve => {
+        image.onload = () => {
+          resolve({
+            name: name,
+            dimensions: { width: image.width, height: image.height }
+          })
+        }
+      })
     })
   )
 }
@@ -108,55 +119,89 @@ const AppBar = ({
     [imageRange, syncAction, setActiveImage]
   )
 
-  const handleExportYOLO = useCallback(async () => {
-    const zip = new JSZip()
-    const folder = zip.folder(bucket)
-    await zipImages(bucket, collection, folder)
+  const handleExportYOLO = useCallback(
+    async e => {
+      e.stopPropagation()
+      setOptionsOpen(false)
+      const zip = new JSZip()
+      const folder = zip.folder(bucket)
+      const images = await zipImages(bucket, collection, folder)
 
-    Object.keys(collection.annotations).forEach(name => {
-      const annotationTxt = collection.annotations[name]
-        .map(annotation => {
-          const labelIndex = collection.labels.indexOf(annotation.label)
-          const width = (annotation.x2 - annotation.x).toFixed(6)
-          const height = (annotation.y2 - annotation.y).toFixed(6)
-          const midX = (annotation.x + width / 2).toFixed(6)
-          const midY = (annotation.y + height / 2).toFixed(6)
-          return `${labelIndex} ${midX} ${midY} ${width} ${height}`
+      images.forEach(({ name }) => {
+        const annotationTxt = collection.annotations[name]
+          .map(annotation => {
+            const labelIndex = collection.labels.indexOf(annotation.label)
+            const width = (annotation.x2 - annotation.x).toFixed(6)
+            const height = (annotation.y2 - annotation.y).toFixed(6)
+            const midX = (annotation.x + width / 2).toFixed(6)
+            const midY = (annotation.y + height / 2).toFixed(6)
+            return `${labelIndex} ${midX} ${midY} ${width} ${height}`
+          })
+          .join('\n')
+        folder.file(
+          `${name.replace(/\.(jpg|jpeg|png)$/i, '')}.txt`,
+          annotationTxt
+        )
+      })
+
+      const zipBlob = await zip.generateAsync({ type: 'blob' })
+      saveAs(zipBlob, `${bucket}.zip`)
+    },
+    [bucket, collection]
+  )
+
+  const handleExportCreateML = useCallback(
+    async e => {
+      e.stopPropagation()
+      setOptionsOpen(false)
+      const zip = new JSZip()
+      const folder = zip.folder(bucket)
+      const images = await zipImages(bucket, collection, folder)
+
+      const createMLAnnotations = images.map(({ name, dimensions }) => ({
+        image: name,
+        annotations: collection.annotations[name].map(annotation => {
+          const relWidth = annotation.x2 - annotation.x
+          const relHeight = annotation.y2 - annotation.y
+          const midX = (annotation.x + relWidth / 2) * dimensions.width
+          const midY = (annotation.y + relHeight / 2) * dimensions.height
+          const width = relWidth * dimensions.width
+          const height = relHeight * dimensions.height
+          return {
+            label: annotation.label,
+            coordinates: {
+              x: Math.round(midX),
+              y: Math.round(midY),
+              width: Math.round(width),
+              height: Math.round(height)
+            }
+          }
         })
-        .join('\n')
-      folder.file(
-        `${name.replace(/\.(jpg|jpeg|png)$/i, '')}.txt`,
-        annotationTxt
-      )
-    })
+      }))
+      folder.file('annotations.json', JSON.stringify(createMLAnnotations))
 
-    const zipBlob = await zip.generateAsync({ type: 'blob' })
-    saveAs(zipBlob, `${bucket}.zip`)
-  }, [bucket, collection])
+      const zipBlob = await zip.generateAsync({ type: 'blob' })
+      saveAs(zipBlob, `${bucket}.zip`)
+    },
+    [bucket, collection]
+  )
 
-  const handleExportCreateML = useCallback(async () => {
-    const zip = new JSZip()
-    const folder = zip.folder(bucket)
-    await zipImages(bucket, collection, folder)
+  const handleExportVOC = useCallback(
+    async e => {
+      e.stopPropagation()
+      setOptionsOpen(false)
+      const zip = new JSZip()
+      const folder = zip.folder(bucket)
+      await zipImages(bucket, collection, folder)
 
-    const collectionJson = collection.toJSON()
-    folder.file('_annotations.json', JSON.stringify(collectionJson))
+      const collectionJson = collection.toJSON()
+      folder.file('_annotations.json', JSON.stringify(collectionJson))
 
-    const zipBlob = await zip.generateAsync({ type: 'blob' })
-    saveAs(zipBlob, `${bucket}.zip`)
-  }, [bucket, collection])
-
-  const handleExportVOC = useCallback(async () => {
-    const zip = new JSZip()
-    const folder = zip.folder(bucket)
-    await zipImages(bucket, collection, folder)
-
-    const collectionJson = collection.toJSON()
-    folder.file('_annotations.json', JSON.stringify(collectionJson))
-
-    const zipBlob = await zip.generateAsync({ type: 'blob' })
-    saveAs(zipBlob, `${bucket}.zip`)
-  }, [bucket, collection])
+      const zipBlob = await zip.generateAsync({ type: 'blob' })
+      saveAs(zipBlob, `${bucket}.zip`)
+    },
+    [bucket, collection]
+  )
 
   return (
     <div className={styles.wrapper}>
