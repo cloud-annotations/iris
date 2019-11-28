38fe5b3e85769d88c9fc856f3387b8c09c35ed67
Nick <bourdakos1@gmail.com>
2019-11-28 18:19:16 -0500

    more styles

--- client/src/App/Localization/AutoLabelPanel.js
+++ client/src/App/Localization/AutoLabelPanel.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react'
+import { InlineLoading } from 'carbon-components-react'
 
 import styles from './AutoLabelPanel.module.css'
 
@@ -7,31 +8,48 @@ import objectDetector from '@cloud-annotations/object-detection'
 const MODEL_PATH =
   '/api/proxy/s3.us-west.cloud-object-storage.test.appdomain.cloud/funky/model_web'
 
-const Expanded = ({ handleClick }) => {
+const MagicIcon = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.button} onClick={handleClick}>
-        <div className={styles.buttonText}>Auto label</div>
-        {/* <CreateIconV2 /> */}
-      </div>
-      <div className={styles.button} onClick={handleClick}>
-        <div className={styles.buttonText}>Auto label</div>
-        {/* <CreateIconV2 /> */}
-      </div>
-      <div className={styles.button} onClick={handleClick}>
-        <div className={styles.buttonText}>Auto label</div>
-        {/* <CreateIconV2 /> */}
-      </div>
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 37.99 33.99"
+      className={styles.buttonIcon}
+    >
+      <path
+        d="M26,12a3.49,3.49,0,0,0-4.95,0L2,31A3.5,3.5,0,0,0,7,36L26,17A3.49,3.49,0,0,0,26,12Zm-1,4-4.5,4.5-3-3L22,13a2.12,2.12,0,0,1,3,3Z"
+        transform="translate(-1.01 -3)"
+      />
+      <path
+        d="M25,10a6.84,6.84,0,0,0,7-7,6.84,6.84,0,0,0,7,7,6.84,6.84,0,0,0-7,7A6.84,6.84,0,0,0,25,10Z"
+        transform="translate(-1.01 -3)"
+      />
+      <path
+        d="M26,23a4.88,4.88,0,0,0,5-5,4.88,4.88,0,0,0,5,5,4.88,4.88,0,0,0-5,5A4.88,4.88,0,0,0,26,23Z"
+        transform="translate(-1.01 -3)"
+      />
+      <path
+        d="M5,10a6.84,6.84,0,0,0,7-7,6.84,6.84,0,0,0,7,7,6.84,6.84,0,0,0-7,7A6.84,6.84,0,0,0,5,10Z"
+        transform="translate(-1.01 -3)"
+      />
+    </svg>
   )
 }
 
-const Collapsed = ({ handleClick }) => {
+const Expanded = ({ handleClick }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.button} onClick={handleClick}>
-        <div className={styles.buttonText}>Auto label</div>
-        {/* <CreateIconV2 /> */}
+      <div className={styles.titleBar}>
+        <div className={styles.title}>Auto label</div>
+        <div className={styles.done}>Done</div>
+      </div>
+      <div className={styles.buttonLabel}>
+        <div className={styles.buttonText}>Label</div>
+      </div>
+      <div className={styles.buttonLabelAll}>
+        <div className={styles.buttonText}>Label all</div>
+      </div>
+      <div className={styles.buttonNext} onClick={handleClick}>
+        <div className={styles.buttonText}>Next</div>
       </div>
     </div>
   )
@@ -42,7 +60,20 @@ const LoadingModel = ({ handleClick }) => {
     <div className={styles.wrapper}>
       <div className={styles.buttonLoading} onClick={handleClick}>
         <div className={styles.buttonText}>Loading model</div>
-        {/* <CreateIconV2 /> */}
+        <div className={styles.loading}>
+          <InlineLoading success={false} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Collapsed = ({ handleClick }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.buttonAutoLabel} onClick={handleClick}>
+        <div className={styles.buttonText}>Auto label</div>
+        <MagicIcon />
       </div>
     </div>
   )
