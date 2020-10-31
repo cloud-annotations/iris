d3d9343f6eeee6ed7e378f01187c5214eb7ca06a
Nick Bourdakos <bourdakos1@gmail.com>
2020-10-31 19:32:56 -0400

    Rename

--- packages/iris-components/src/CrossHair/index.stories.tsx
+++ packages/iris-components/src/CrossHair/index.stories.tsx
@@ -5,7 +5,7 @@ import { Story } from "@storybook/react/types-6-0";
 import CrossHair from "./";
 
 export default {
-  title: "Example/CrossHair",
+  title: "Example/CrossHairV2",
   component: CrossHair,
 };
 
--- packages/iris-components/src/CrossHair/index.tsx
+++ packages/iris-components/src/CrossHair/index.tsx
@@ -1,22 +1,9 @@
 import React, { useRef, useEffect, useCallback } from "react";
 
-import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import { createStyles, makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles((theme: Theme) => {
-  const baseStyles = createStyles({
-    hair: {
-      visibility: "hidden",
-      position: "absolute",
-      top: 0,
-      left: 0,
-      marginTop: -1,
-      background: "transparent",
-      pointerEvents: "none",
-      zIndex: 2,
-    },
-  });
-
-  return createStyles({
+const useStyles = makeStyles(() =>
+  createStyles({
     wrapper: {
       position: "absolute",
       top: 0,
@@ -25,81 +12,18 @@ const useStyles = makeStyles((theme: Theme) => {
       bottom: 0,
       userSelect: "none",
       cursor: "none",
+      overflow: "hidden",
     },
-    hairH: {
-      ...baseStyles.hair,
-      borderTop: "1px solid rgb(60, 255, 0)",
-      borderLeft: "1px solid rgb(60, 255, 0)",
-      left: 0,
-      right: 0,
-    },
-    hairHShaddow: {
-      ...baseStyles.hair,
-      mixBlendMode: "difference",
-      borderTop: "1px solid rgba(255, 255, 255, 0.2)",
-      borderLeft: "1px solid rgba(255, 255, 255, 0.2)",
-      left: 0,
-      right: 0,
-    },
-    hairV: {
-      ...baseStyles.hair,
-      borderTop: "1px solid rgb(148, 148, 148)",
-      borderLeft: "1px solid rgb(148, 148, 148)",
-      top: 0,
-      bottom: 0,
-    },
-    hairVShaddow: {
-      ...baseStyles.hair,
-      mixBlendMode: "difference",
-      borderTop: "1px solid rgba(255, 255, 255, 0.2)",
-      borderLeft: "1px solid rgba(255, 255, 255, 0.2)",
-      top: 0,
-      bottom: 0,
-    },
-    hairDot: {
+    base: {
       visibility: "hidden",
       position: "absolute",
-      background: "#000000",
-      marginLeft: -1,
-      marginTop: -2,
-      border: "1px solid rgba(255, 255, 255, 0.5)",
-      width: 1,
-      height: 1,
-      borderRadius: 1.5,
-      boxSizing: "content-box",
-      backgroundClip: "padding-box",
       pointerEvents: "none",
+      zIndex: 2,
     },
-    hairCenterH: {
-      visibility: "hidden",
-      position: "absolute",
-      background: "#000000",
-      marginTop: -2,
-      marginLeft: -1,
-      border: "1px solid rgba(255, 255, 255, 0.5)",
-      width: 5,
-      height: 1,
-      borderRadius: 1.5,
-      boxSizing: "content-box",
-      backgroundClip: "padding-box",
-      pointerEvents: "none",
-    },
-    hairCenterV: {
-      visibility: "hidden",
-      position: "absolute",
-      background: "#000000",
-      marginTop: -2,
-      marginLeft: -1,
-      border: "1px solid rgba(255, 255, 255, 0.5)",
-      width: 1,
-      height: 5,
-      borderRadius: 1.5,
-      boxSizing: "content-box",
-      backgroundClip: "padding-box",
-      pointerEvents: "none",
-    },
-  });
-});
+    shadow: { fill: "rgba(255, 255, 255, 0.2)" },
+    border: { fill: "rgba(255, 255, 255, 0.5)" },
+  })
+);
 
 interface Props {
   active?: boolean;
@@ -107,127 +31,134 @@ interface Props {
   color?: string;
 }
 
+const Pointer = React.forwardRef(({ color }: any, ref: any) => {
+  const classes = useStyles();
+  return (
+    <svg viewBox="0 0 38 38" ref={ref} className={classes.base}>
+      <g className={classes.border}>
+        <rect x="0" y="16" width="14" height="6" rx="3" />
+        <rect x="24" y="16" width="14" height="6" rx="3" />
+        <rect x="16" y="0" width="6" height="14" rx="3" />
+        <rect x="16" y="24" width="6" height="14" rx="3" />
+        <rect x="16" y="16" width="6" height="6" rx="3" />
+      </g>
+
+      <g style={{ fill: color }}>
+        <rect x="2" y="18" width="10" height="2" />
+        <rect x="26" y="18" width="10" height="2" />
+        <rect x="18" y="2" width="2" height="10" />
+        <rect x="18" y="26" width="2" height="10" />
+        <rect x="18" y="18" width="2" height="2" />
+      </g>
+    </svg>
+  );
+});
+
+const Horizontal = React.forwardRef(({ color }: any, ref: any) => {
+  const classes = useStyles();
+  return (
+    <svg
+      preserveAspectRatio="none"
+      viewBox="0 0 1 2"
+      className={classes.base}
+      ref={ref}
+    >
+      <rect x="0" y="0" width="1" height="1" style={{ fill: color }} />
+      <rect x="0" y="1" width="1" height="1" className={classes.shadow} />
+    </svg>
+  );
+});
+
+const Vertical = React.forwardRef(({ color }: any, ref: any) => {
+  const classes = useStyles();
+  return (
+    <svg
+      preserveAspectRatio="none"
+      viewBox="0 0 2 1"
+      className={classes.base}
+      ref={ref}
+    >
+      <rect x="0" y="0" width="1" height="1" style={{ fill: color }} />
+      <rect x="1" y="0" width="1" height="1" className={classes.shadow} />
+    </svg>
+  );
+});
+
+function setStyle(el: HTMLElement | SVGElement | null, style: any) {
+  if (el) {
+    for (const [key, val] of Object.entries(style)) {
+      (el.style as any)[key] = val;
+    }
+  }
+}
+
 function CrossHair({ active, children, color }: Props) {
   const classes = useStyles();
 
   const wrapperRef = useRef<HTMLDivElement>(null);
 
-  const centerDotRef = useRef<HTMLDivElement>(null);
-
-  const centerLeftRef = useRef<HTMLDivElement>(null);
-  const centerRightRef = useRef<HTMLDivElement>(null);
-  const centerTopRef = useRef<HTMLDivElement>(null);
-  const centerBottomRef = useRef<HTMLDivElement>(null);
-
-  const crosshairLeftShaddowRef = useRef<HTMLDivElement>(null);
-  const crosshairRightShaddowRef = useRef<HTMLDivElement>(null);
-  const crosshairTopShaddowRef = useRef<HTMLDivElement>(null);
-  const crosshairBottomShaddowRef = useRef<HTMLDivElement>(null);
-
-  const crosshairLeftRef = useRef<HTMLDivElement>(null);
-  const crosshairRightRef = useRef<HTMLDivElement>(null);
-  const crosshairTopRef = useRef<HTMLDivElement>(null);
-  const crosshairBottomRef = useRef<HTMLDivElement>(null);
+  const pointerRef = useRef<SVGSVGElement>(null);
+  const leftRef = useRef<any>(null);
+  const rightRef = useRef<SVGSVGElement>(null);
+  const topRef = useRef<SVGSVGElement>(null);
+  const bottomRef = useRef<SVGSVGElement>(null);
 
   const setVisibility = useCallback((visibility: "visible" | "hidden") => {
-    if (
-      centerDotRef.current &&
-      centerLeftRef.current &&
-      centerRightRef.current &&
-      centerTopRef.current &&
-      centerBottomRef.current &&
-      crosshairLeftShaddowRef.current &&
-      crosshairRightShaddowRef.current &&
-      crosshairTopShaddowRef.current &&
-      crosshairBottomShaddowRef.current &&
-      crosshairLeftRef.current &&
-      crosshairRightRef.current &&
-      crosshairTopRef.current &&
-      crosshairBottomRef.current
-    ) {
-      centerDotRef.current.style.visibility = visibility;
-
-      centerLeftRef.current.style.visibility = visibility;
-      centerRightRef.current.style.visibility = visibility;
-      centerTopRef.current.style.visibility = visibility;
-      centerBottomRef.current.style.visibility = visibility;
-
-      crosshairLeftShaddowRef.current.style.visibility = visibility;
-      crosshairRightShaddowRef.current.style.visibility = visibility;
-      crosshairTopShaddowRef.current.style.visibility = visibility;
-      crosshairBottomShaddowRef.current.style.visibility = visibility;
-
-      crosshairLeftRef.current.style.visibility = visibility;
-      crosshairRightRef.current.style.visibility = visibility;
-      crosshairTopRef.current.style.visibility = visibility;
-      crosshairBottomRef.current.style.visibility = visibility;
-    }
+    setStyle(pointerRef.current, { visibility });
+    setStyle(leftRef.current, { visibility });
+    setStyle(rightRef.current, { visibility });
+    setStyle(topRef.current, { visibility });
+    setStyle(bottomRef.current, { visibility });
   }, []);
 
   useEffect(() => {
-    const handleMouseMove = (e: any) => {
-      if (!wrapperRef.current) {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (
+        !wrapperRef.current ||
+        !wrapperRef.current.contains(e.target as Node)
+      ) {
         return;
-      }
-
-      if (wrapperRef.current.contains(e.target)) {
-        setVisibility("visible");
       }
 
       const rect = wrapperRef.current.getBoundingClientRect();
       const mX = e.clientX - rect.left;
       const mY = e.clientY - rect.top;
 
-      if (
-        centerDotRef.current &&
-        centerLeftRef.current &&
-        centerRightRef.current &&
-        centerTopRef.current &&
-        centerBottomRef.current &&
-        crosshairLeftShaddowRef.current &&
-        crosshairRightShaddowRef.current &&
-        crosshairTopShaddowRef.current &&
-        crosshairBottomShaddowRef.current &&
-        crosshairLeftRef.current &&
-        crosshairRightRef.current &&
-        crosshairTopRef.current &&
-        crosshairBottomRef.current
-      ) {
-        centerDotRef.current.style.top = `${mY}px`;
-        centerDotRef.current.style.left = `${mX}px`;
+      setStyle(pointerRef.current, {
+        top: mY - 9.5,
+        left: mX - 9.5,
+        width: 19,
+        height: 19,
+      });
 
-        centerLeftRef.current.style.top = `${mY}px`;
-        centerLeftRef.current.style.left = `${mX - 8}px`;
-        centerRightRef.current.style.top = `${mY}px`;
-        centerRightRef.current.style.left = `${mX + 4}px`;
+      setStyle(leftRef.current, {
+        top: mY,
+        left: 0,
+        width: mX - 11,
+        height: 2,
+      });
 
-        centerTopRef.current.style.left = `${mX}px`;
-        centerTopRef.current.style.top = `${mY - 8}px`;
-        centerBottomRef.current.style.left = `${mX}px`;
-        centerBottomRef.current.style.top = `${mY + 4}px`;
+      setStyle(rightRef.current, {
+        top: mY,
+        left: mX + 12,
+        width: rect.width - (mX + 12),
+        height: 2,
+      });
 
-        // Shaddow
-        crosshairLeftShaddowRef.current.style.top = `${mY + 1}px`;
-        crosshairRightShaddowRef.current.style.top = `${mY + 1}px`;
-        crosshairLeftShaddowRef.current.style.width = `${mX - 11}px`;
-        crosshairRightShaddowRef.current.style.left = `${mX + 12}px`;
+      setStyle(topRef.current, {
+        left: mX,
+        width: 2,
+        height: mY - 11,
+      });
 
-        crosshairTopShaddowRef.current.style.left = `${mX + 1}px`;
-        crosshairTopShaddowRef.current.style.height = `${mY - 11}px`;
-        crosshairBottomShaddowRef.current.style.left = `${mX + 1}px`;
-        crosshairBottomShaddowRef.current.style.top = `${mY + 12}px`;
+      setStyle(bottomRef.current, {
+        top: mY + 12,
+        left: mX,
+        width: 2,
+        height: rect.height - (mY + 12),
+      });
 
-        // Highlight
-        crosshairLeftRef.current.style.top = `${mY}px`;
-        crosshairRightRef.current.style.top = `${mY}px`;
-        crosshairLeftRef.current.style.width = `${mX - 11}px`;
-        crosshairRightRef.current.style.left = `${mX + 12}px`;
-
-        crosshairTopRef.current.style.left = `${mX}px`;
-        crosshairTopRef.current.style.height = `${mY - 11}px`;
-        crosshairBottomRef.current.style.left = `${mX}px`;
-        crosshairBottomRef.current.style.top = `${mY + 12}px`;
-      }
+      setVisibility("visible");
     };
 
     document.addEventListener("mousemove", handleMouseMove);
@@ -236,91 +167,31 @@ function CrossHair({ active, children, color }: Props) {
     };
   }, [setVisibility]);
 
-  const handleMouseEnter = useCallback(() => {
-    setVisibility("visible");
-  }, [setVisibility]);
-
   const handleMouseLeave = useCallback(() => {
     setVisibility("hidden");
   }, [setVisibility]);
 
   if (!active) {
-    return <div>{children}</div>;
+    return (
+      <div draggable={false} className={classes.wrapper}>
+        {children}
+      </div>
+    );
   }
+
   return (
     <div
       draggable={false}
       ref={wrapperRef}
       onMouseLeave={handleMouseLeave}
-      onMouseEnter={handleMouseEnter}
       className={classes.wrapper}
     >
       {children}
-
-      <div
-        style={{ background: `padding-box ${color}` }}
-        ref={centerDotRef}
-        className={classes.hairDot}
-      />
-
-      <div
-        style={{ background: `padding-box ${color}` }}
-        ref={centerLeftRef}
-        className={classes.hairCenterH}
-      />
-      <div
-        style={{ background: `padding-box ${color}` }}
-        ref={centerRightRef}
-        className={classes.hairCenterH}
-      />
-      <div
-        style={{ background: `padding-box ${color}` }}
-        ref={centerTopRef}
-        className={classes.hairCenterV}
-      />
-      <div
-        style={{ background: `padding-box ${color}` }}
-        ref={centerBottomRef}
-        className={classes.hairCenterV}
-      />
-
-      <div ref={crosshairLeftShaddowRef} className={classes.hairHShaddow} />
-      <div ref={crosshairRightShaddowRef} className={classes.hairHShaddow} />
-      <div ref={crosshairTopShaddowRef} className={classes.hairVShaddow} />
-      <div ref={crosshairBottomShaddowRef} className={classes.hairVShaddow} />
-
-      <div
-        style={{
-          borderTop: `1px solid ${color}`,
-          borderLeft: `1px solid ${color}`,
-        }}
-        ref={crosshairLeftRef}
-        className={classes.hairH}
-      />
-      <div
-        style={{
-          borderTop: `1px solid ${color}`,
-          borderLeft: `1px solid ${color}`,
-        }}
-        ref={crosshairRightRef}
-        className={classes.hairH}
-      />
-      <div
-        style={{
-          borderTop: `1px solid ${color}`,
-          borderLeft: `1px solid ${color}`,
-        }}
-        ref={crosshairTopRef}
-        className={classes.hairV}
-      />
-      <div
-        style={{
-          borderTop: `1px solid ${color}`,
-          borderLeft: `1px solid ${color}`,
-        }}
-        ref={crosshairBottomRef}
-        className={classes.hairV}
-      />
+      <Pointer color={color} ref={pointerRef} />
+      <Horizontal color={color} ref={leftRef} />
+      <Horizontal color={color} ref={rightRef} />
+      <Vertical color={color} ref={topRef} />
+      <Vertical color={color} ref={bottomRef} />
     </div>
   );
 }
